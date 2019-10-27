e216210136b1f7cb6ec37cb69c7e885c4e50679c
nehaarora97 <57046381+nehaarora97@users.noreply.github.com>
2019-10-27 17:35:02 +0530

    Update Bubble Sort.c

--- sorting/Bubble Sort.c
+++ sorting/Bubble Sort.c
@@ -5,7 +5,7 @@ int main(){
 	int length, i, k, num[1000], aux;
 	char order;
 
-	printf("What is the length of your sequence?\nType here: ");
+	printf("What is the length of your sequence?\nType here:- ");
 	scanf("%d", &length);
 	printf("Type the numbers that you want to order:\n");
 	for(i=0; i<length; i++){
